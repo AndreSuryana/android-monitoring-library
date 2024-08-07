3df5e6fc0f24ff21e63d4a00e95651aaef285e46
Andre Suryana <kadek.andre@primavista-solusi.com>
2024-08-07 14:27:42 +0700

    Docs: Add Javadoc comments for classes inside `:logging` module
    Details
    - This improves code documentation and helps developers understand the usage and functionality of components under `:logging` module.
    - Removed unused LogData

--- logging/src/main/java/com/andresuryana/amlib/logging/prefs/LoggingSharedPreferences.kt
+++ logging/src/main/java/com/andresuryana/amlib/logging/prefs/LoggingSharedPreferences.kt
@@ -3,19 +3,40 @@ package com.andresuryana.amlib.logging.prefs
 import android.content.Context
 import android.content.SharedPreferences
 
-class LoggingSharedPreferences(context: Context) {
+/**
+ * A class for managing logging-related preferences using [SharedPreferences].
+ * This class provides methods to read and write preferences related to logging configuration.
+ *
+ * @param context The context used to access the [SharedPreferences] instance.
+ */
+class LoggingSharedPreferences private constructor(context: Context) {
 
     private val prefs: SharedPreferences =
         context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
 
+    /**
+     * The device ID used for identifying the device in log messages.
+     *
+     * @return The device ID if set, otherwise `null`.
+     */
     var deviceId: String?
         get() = prefs.getString(KEY_DEVICE_ID, null)
         set(value) = prefs.edit().putString(KEY_DEVICE_ID, value).apply()
 
+    /**
+     * The priority of the log level used to filter log messages.
+     *
+     * @return The current log level priority. Defaults to -1 if not set.
+     */
     var logLevelPriority: Int
         get() = prefs.getInt(KEY_LOG_LEVEL, -1)
         set(value) = prefs.edit().putInt(KEY_LOG_LEVEL, value).apply()
 
+    /**
+     * Whether console logging is enabled.
+     *
+     * @return `true` if console logging is enabled; `false` otherwise.
+     */
     var isConsoleLogging: Boolean
         get() = prefs.getBoolean(KEY_CONSOLE_LOGGING, false)
         set(value) = prefs.edit().putBoolean(KEY_CONSOLE_LOGGING, value).apply()
@@ -30,6 +51,12 @@ class LoggingSharedPreferences(context: Context) {
         @Volatile
         private var INSTANCE: LoggingSharedPreferences? = null
 
+        /**
+         * Returns a singleton instance of [LoggingSharedPreferences].
+         *
+         * @param context The context used to create the [LoggingSharedPreferences] instance if it does not exist.
+         * @return The singleton [LoggingSharedPreferences] instance.
+         */
         fun getInstance(context: Context): LoggingSharedPreferences =
             INSTANCE ?: synchronized(this) {
                 INSTANCE ?: LoggingSharedPreferences(context).also { INSTANCE = it }
